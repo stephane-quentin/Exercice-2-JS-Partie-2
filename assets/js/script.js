7ea9ce10235fe5f2ed23fb2d47e952869be2ef97
Stiouf <stephane.quentin@live.fr>
2018-10-08 16:16:35 +0200

    ex2

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function ex2(){
+  var name = document.getElementById('lastname').value;
+  if (isNaN(name) || name!=''){
+  alert('Merci de votre participation ' + name);
+} else{
+  alert('Erreur de caractères !');
+}
+}
