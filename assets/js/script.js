9983e4608248b7f22390ddf0e8efaab086b3adf1
Stiouf <stephane.quentin@live.fr>
2018-10-09 09:21:32 +0200

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 function ex2(){
   var name = document.getElementById('lastname').value;
-  if (isNaN(name) && name!=''){
+  if (isNaN(name) && name!=''){ // si la variable name n'est pas un chiffre et nn'est pas vide
     alert('Merci de votre participation ' + name);
 } else {
     alert('Erreur de caractères !');
