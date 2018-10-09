10e2bf1f80a24d23b34d6ef121d951d9edf9a4cf
Stiouf <stephane.quentin@live.fr>
2018-10-09 09:20:25 +0200

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 function ex2(){
   var name = document.getElementById('lastname').value;
-  if (isNaN(name) || name!=''){
-  alert('Merci de votre participation ' + name);
-} else{
-  alert('Erreur de caractères !');
+  if (isNaN(name) && name!=''){
+    alert('Merci de votre participation ' + name);
+} else {
+    alert('Erreur de caractères !');
 }
 }
